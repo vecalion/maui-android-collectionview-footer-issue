7c2b4c519fe58159087b27039ece3cb8d04c84b0
Valeriy Kovalenko <valeriy.kovalenko@squarecrowdapps.com>
2024-02-17 16:13:15 +0200

    initial

---
+++ CollectionViewAndroidIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionViewAndroidIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ CollectionViewAndroidIssue/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace CollectionViewAndroidIssue;
+
+public class MainViewModel
+{
+    public ObservableCollection<string> Items { get; set; } = new()
+    {
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+    };
+
+    public ICommand RemoveItemCommand => new Command(RemoveItem);
+    public ICommand AddItemCommand => new Command(AddItem);
+
+    void RemoveItem()
+    {
+        if (Items.Count > 0)
+        {
+            Items.Remove(Items.Last());
+        }
+    }
+
+    void AddItem()
+    {
+        Items.Add($"Item {Items.Count + 1}");
+    }
+}
+
